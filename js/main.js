a2787b375e24b66c17bb2bc18c81d8ed2a2e3995
ZoeAde <zoe.a.adelman@gmail.com>
2015-08-07 16:04:36 -0600

    adding board function and methods

--- js/main.js
+++ js/main.js
@@ -1,8 +1,9 @@
 //Create a Game() class that creates instances of the Board() class, Players() class (you need two players) and has a turnCounter property to indicate whose turn it is and determine the winner in case of a draw.
 
 var Game = function(player1, player2) {
-  this.player1 = playerX;
-  this.player2 = playerO;
+  this.board = new Board();
+  this.player1 = new Player();
+  this.player2 = new Player();
   this.turnCounter = 1;
 };
 
@@ -11,18 +12,15 @@ var Game = function(player1, player2) {
 Game.prototype.nextPlayer = function() {
   if (this.turnCounter === 1) {
     this.turnCounter = 2;
-    return this.player1;
   }
   else if (this.turnCounter === 2) {
     this.turnCounter = 1;
-    return this.player2;
   }
 };
 
-// updateScore() - updates the scoreboard based on the playerScore from the Player() class. Updates the DOM.
-Game.prototype.updateScore = function() {
+//method to determine whose turn it is based on turn counter
 
-};
+
 
 // init() - initializes a new game with a board and two players. Make sure to add a click event handler for each of the board's cells on the DOM. This should in turn call the makeMove() method on the Board() class.
 Game.prototype.init = function() {
@@ -37,35 +35,63 @@ var Player = function(team) {
   this.playerScore = 0;
 };
 
-// Create a Board() class that has a 'moveArr' property with a null array to hold the moves along with a $cells property to assign each cell to a box in the DOM. Finally, add an event handler on the reset button to call the resetBoard() method.
-var Board = function() {
-  this.moveArr = [];
-  this.$(cells) = null;
+// updateScore() - updates the scoreboard based on the playerScore from the Player() class. Updates the DOM.
+Player.prototype.updateScore = function() {
+  this.playerScore ++;
 };
 
+// Create a Board() class that has a 'moveArr' property with a null array to hold the moves along with a $cells property to assign each cell to a box in the DOM. Finally, add an event handler on the reset button to call the resetBoard() method.
+var Board = function() {
+  this.moveArr = [null, null, null, null, null, null, null, null, null];
+  this.$cells = $('.box');
+};
+
+Board.prototype.resetBoard = function() {
+  this.moveArr = [null, null, null, null, null, null, null, null, null];
+  //reset score, turncounter, players, reset dom
+
+};
 // makeMove() - sets a box to a player/team and updates the moveArr array. Make sure to prevent occupied boxes from being used (alert the user).
 Board.prototype.makeMove() = function() {
-
+//event that adds x or o to board on dom
+//this.moveArr[ID FROM DOM] = EITHER X OR O
 }
 
 
 // winCondition() - array which holds the winining conditions for the board
 Board.prototype.winCondition = function() {
   var winningArr = {
+    //arrau of possible winning hands
 };
 
 // checkWinner() - checks the board for winning combinations against the values of each player/team and alerts the players which player won or if it's a tie
 Board.prototype.checkWinner = function() {
-
+//check only on players turn
 };
 
 // resetBoard() - resets the board to its initial state and resets the moveArr holding the player values (via the nullArray() method) and turn counter
-Board.prototype.resetBoard = function() {
-  nullArray();
-  $(cells) = null;
-};
+
 
 // nullArray() - resets the moveArr
 Board.prototype.nullArray = function() {
   this.moveArr = [];
 };
+
+
+
+$(document).on('ready', function() {
+
+//call reset board on instance
+$('.reset').on('click', function() {
+
+  });
+
+
+});
+
+//7 ways to win, array of arrays
+/*
+[0,1,2
+0,3,6
+
+*/
